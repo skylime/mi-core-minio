8ef542a284e09a9e15571ead797a8bb862485df4
Thomas Merkel <tm@core.io>
2020-07-29 11:47:23 +0200

    Add extra minio user

--- copy/var/zoneinit/includes/05-delegated-dataset.sh
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -11,4 +11,8 @@ if zfs list ${DDS} 1>/dev/null 2>&1; then
 	znapzendzetup create --recursive --tsformat='%Y-%m-%d-%H%M%S' --donotask \
 		SRC '7day=>8hour,30day=>1day,1year=>1week,10year=>1month' ${DDS}
 	/usr/sbin/svcadm enable svc:/pkgsrc/znapzend:default
+else
+	mkdir -p /data/minio
 fi
+
+chown minio:minio /data/minio
